655a6e5c0346985919a0d29b5666a005a1e0db37
qiualu <1101938064@qq.com>
2019-04-04 18:06:01 +0800

    V0.002 清明回家接着敲代码

--- baseApp/views.py
+++ baseApp/views.py
@@ -11,8 +11,12 @@ from django.shortcuts import render_to_response
 # def page_not_found(request):
 #     return render_to_response('baseApp/404.html')
 
+# def index(request):
+#     return HttpResponse(u"欢迎光临 自强学堂!")
+
 def index(request):
     return render(request, 'baseApp/404.html')
+    # return render(request, 'home.html')
 
 # @csrf_exempt
 # def page_error(request):
