c040e5e29ca529b1da9f584c37addd2b7e7b7ef8
1101938064@qq.com <1101938064@qq.com>
2019-09-29 19:54:05 +0800

    bu - v 0.004

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 # 当运行 python manage.py collectstatic 的时候
 # STATIC_ROOT 文件夹 是用来将所有STATICFILES_DIRS中所有文件夹中的文件，以及各app中static中的文件都复制过来
 # 把这些文件放到一起是为了用apache等部署的时候更方便
-# STATIC_ROOT = os.path.join(BASE_DIR, os.path.join('templates', 'static'))
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 # 二选一
 
